5dbb42f0be5c655da9fc8799394ebe75dc94550c
madpaddad <Daraphal39@gmail.com>
2024-03-16 03:44:35 -0400

    update

---
+++ transition.js
@@ -0,0 +1,13 @@
+document.getElementById('startbooking').addEventListener('click', function() {
+    toggleBookingInfo();
+});
+
+
+function toggleBookingInfo() {
+    var bookinginfo = document.getElementById('bookinginfo');
+    if (bookinginfo.style.display == 'none') {
+        bookinginfo.style.display = 'block';
+    } else {
+        bookinginfo.style.display = 'none';
+    }
+}
